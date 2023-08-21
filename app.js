b2f4b9866cd0f3077093549e5690cc95e122bdfb
AlexHHall <137228056+AlexHHall@users.noreply.github.com>
2023-08-21 15:18:56 +1200

    working

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const app = Express();
 
 app.use(Express.static('public'));
 
+app.get('/dutchblitz', (req, res) => {
+    res.sendFile(__dirname + '/public/dutchblitz.html');
+    console.log('dutchblitz.html');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+    console.log('dutchblitz.html');
+});
+
 app.listen(3000, ()=> {
     console.log('localhost:3000');
 })
